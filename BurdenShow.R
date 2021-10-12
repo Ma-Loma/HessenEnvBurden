26d82124512db140311c89d12f0074f6fb9dfb5b
Matthias Lochmann <59730731+Ma-Loma@users.noreply.github.com>
2021-10-12 12:02:54 +0200

    Fehlerrechnungen berücksichtigt, aber noch nicht die korrekten Korellationen

--- BurdenShow.R
+++ BurdenShow.R
@@ -4,6 +4,9 @@ library(readr)
 library(dplyr)
 library(tidyverse)
 library(purrr)
+#install.packages("errors")
+library(errors)
+
 
 popHessen <-
   6116203#Quelle: Summe der Exponierten nach Kartierungsdokumentation
@@ -15,8 +18,15 @@ doseRes_list <- read_delim(
   locale = locale(date_names = "de", decimal_mark = ","),
   trim_ws = TRUE
 )
+doseRes_list$rrPer10dB <-
+  set_errors(doseRes_list$rrPer10dB,
+             doseRes_list$rrPer10dB - doseRes_list$rrLoCI)
+doseRes_list$linearTerm<-doseRes_list$linearTerm %>% set_errors()
+doseRes_list$constantTerm<-doseRes_list$constantTerm %>% set_errors()
+doseRes_list$quadraticTerm<-doseRes_list$quadraticTerm %>% set_errors()
 
-set <- .Primitive("[[<-")
+set <-
+  .Primitive("[[<-") #eine Hilfsfunktion, die definiert wird, um in Pipes Werte manipulieren zu können.
 
 plus_LDEN <- read_delim(
   "rawdata/PLUS_Lden_Hegewald.csv",
@@ -56,29 +66,6 @@ plus_L24h_red <-
   plus_L24h %>% mutate(Lo = Lo - 3,
                        Hi = Hi - 3,
                        source = "plus-3dB")
-
-# test_LDEN <- read_delim(
-#   "rawdata/Exponierte_Test_LDEN.csv",
-#   ";",
-#   escape_double = FALSE,
-#   locale = locale(date_names = "de",
-#                   decimal_mark = ","),
-#   trim_ws = TRUE
-# ) %>% mutate(noiseMetric = "LDEN", source = "test")
-#
-#
-# test_LDEN_red <-
-#   test_LDEN %>% mutate(Lo = Lo - 3,
-#                        Hi = Hi - 3,
-#                        source = "test-3dB")
-#
-# test_L24h <-
-#   test_LDEN %>% mutate(
-#     Lo = Lo - 3.3,
-#     Hi = Hi - 3.3,
-#     source = "test",
-#     noiseMetric = "L24h"
-#   )
 
 END_LDEN <- read_delim(
   "rawdata/Exponierte_ULR_LDEN.csv",
@@ -135,7 +122,7 @@ pl1 + geom_step(direction = "hv") + #waagrechter Strich bis zum nächsten Pegelw
   facet_grid(cols = vars(noiseMetric)) +
   geom_segment(aes(xend = Hi, yend = ExposedPerdB)) + #geom_segment, um noch das letzte waagrechte hinzuzufügen. Alleine wären keine senkrechten Striche
   ylim(0, 300000) + xlim(40, 85)
-ggsave("graphs/Exposure.pdf")
+#ggsave("graphs/Exposure.pdf")
 
 short_l <- doseRes_list %>% filter(included == TRUE) %>%
   select(
@@ -152,97 +139,120 @@ short_l <- doseRes_list %>% filter(included == TRUE) %>%
     burdenCalculation
   )
 
-linFkt = function(pegel, rr, cf, q, l, c) {
-  # stelle als prozentuale Risikoerhöhung dar
-  (rr - 1) * 10 * pegel - cf *
-    (rr - 1) * 10
+
+
+# paraTransform = function(x_tbl) {
+#   x_tbl$quadraticTerm <- 0
+#   x_tbl$linearTerm <- (x_tbl$rrPer10dB - 1) * 10
+#   x_tbl$constantTerm <-
+#     -(x_tbl$linearTerm*x_tbl$counterfactualValue )
+#   return(x_tbl)
+# }
+# 
+# short_l[is.na(short_l$quadraticTerm), ] <-
+#   paraTransform(short_l[is.na(short_l$quadraticTerm), ])
+
+
+quadr = function(x_tbl) {
+  return(
+    x_tbl$quadraticTerm * x_tbl$Lmid ^ 2 +
+      x_tbl$linearTerm * x_tbl$Lmid +
+      x_tbl$constantTerm
+  )
 }
-quadr = function(pegel, rr, cf, q, l, c) {
-  q * pegel ^ 2 + l *
-    pegel + c
-}
-poly = function(pegel, rr, cf, q, l, c) {
-  if (is.na(q))
-    data.frame(L = pegel, effectSize = pmax(linFkt(pegel, rr, cf, q, l, c), 0))
-  else
-    data.frame(L = pegel, effectSize = quadr(pegel, rr, cf, q, l, c))
+linF = function(x_tbl) {
+  dummy <- case_when(#dieser Befehl ist hier wichtig, damit alle Werte im Vektor einzeln prozessiert werden
+    x_tbl$Lmid > x_tbl$counterfactualValue ~ (x_tbl$rrPer10dB - 1) * 10 * (x_tbl$Lmid - x_tbl$counterfactualValue),
+    x_tbl$Lmid <= x_tbl$counterfactualValue ~ set_errors(0, 0)
+  )
+  return(dummy)
 }
 
-Lstep <- 0.1
+#hier generiere eine Liste mit Auswertestellen (Pegeln), an denen Wirkungen berechnet werden.
+Lstep <- 0.1 #Schrittgröße
 auswertestellen <- seq(40, 80, Lstep)
-doseRespF_data <- c()
-for (j in 1:nrow(short_l)) {
-  doseRespF_data <-
-    poly(
-      auswertestellen,
-      as.numeric(short_l[j, "rrPer10dB"]),
-      as.numeric(short_l[j, "counterfactualValue"]),
-      as.numeric(short_l[j, "quadraticTerm"]),
-      as.numeric(short_l[j, "linearTerm"]),
-      as.numeric(short_l[j, "constantTerm"])
-    )  %>% mutate(short_l[j, c("shortName", "noiseMetric")]) %>% bind_rows(doseRespF_data)
-}
-
+doseRespF_data <-
+  short_l %>% crossing(Lmid = auswertestellen)%>%   mutate(effectSize = set_errors(NA, 0)) %>%
+  mutate(effectSize = case_when(# je nachdem, ob Wert für lineare Steigung verfügbar oder nicht.
+    is.na(rrPer10dB) ~ quadr(.),
+    !is.na(rrPer10dB) ~ linF(.)
+  ))
 pl2 <-
-  ggplot(doseRespF_data, aes(x = L, y = effectSize, color = shortName))
-pl2 + geom_line() + facet_grid(cols = vars(noiseMetric))
+  ggplot(
+    doseRespF_data,
+    aes(
+      x = Lmid,
+      y = effectSize,
+      color = shortName,
+      ymin = errors_min(effectSize),
+      ymax = errors_max(effectSize)
+    )
+  )
+pl2 + geom_line() + facet_grid(cols = vars(noiseMetric)) + 
+  geom_ribbon(aes(fill =shortName, linetype = NA), alpha = 0.15)
 #ggsave("graphs/DRF.pdf")
 
-
 er_list <-
-  left_join(exposure_list, short_l, by = "noiseMetric") %>% mutate(response =
-                                                                     -1000000)
-#Spalte response soll numerisch sein, und noch leer sein, wird gleich gefüllt.
+  left_join(exposure_list, short_l, by = "noiseMetric") %>%mutate(response =  -1000000)
+
+#Spalte response soll numerisch sein
 #Enthält dann entweder %HA, %HSD oder %Risikoerhöhung
-for (j in 1:nrow(er_list)) {
-  er_list[j, "response"] <- poly(
-    as.numeric(er_list[j, "Lmid"]),
-    as.numeric(er_list[j, "rrPer10dB"]),
-    as.numeric(er_list[j, "counterfactualValue"]),
-    as.numeric(er_list[j, "quadraticTerm"]),
-    as.numeric(er_list[j, "linearTerm"]),
-    as.numeric(er_list[j, "constantTerm"])
-  )["effectSize"]
-}
+er_list$response<-case_when(# je nachdem, ob Wert für lineare Steigung verfügbar oder nicht.
+             is.na(er_list$rrPer10dB) ~ quadr(er_list),
+             !is.na(er_list$rrPer10dB) ~ linF(er_list))
+
 er_list <- er_list %>% mutate(affected_i = Exposed * response / 100)
 
 dw_list <-
   data.frame(outcome = c("Annoyance", "Sleep disorders"),
              dw = c(0.02, 0.07))
 
-streetNoiseBurden1 <- er_list %>%
-  filter(burdenCalculation == "paf") %>% aggregate(affected_i ~ source + noiseMetric + shortName +
-                                                     outcome,
-                                                   data = .,
-                                                   sum) %>%
-  mutate(PAF = affected_i / popHessen / (1 + affected_i / popHessen) *
+
+streetNoiseBurden1 <-
+  er_list %>% filter(burdenCalculation == "paf")  %>% 
+  mutate(PAF_i = affected_i / popHessen / (1 + affected_i / popHessen) *
            100,
          .keep = "unused") %>%
+  group_by(source, noiseMetric, shortName,outcome) %>%
+  summarise(PAF = sum(PAF_i))%>%
   left_join(., data.frame(filter(
     burden_list, population ==
       "Hessians aged over 40 years"
   )),
-  by = "outcome") %>%
+  by = "outcome")%>%
   left_join(., dw_list, by = "outcome") %>%
-  mutate(attrBurden = PAF * burden / 100) %>% 
-  select(source, noiseMetric, shortName,outcome, PAF, attrBurden) %>% 
-  mutate(dw = NA ,HAorHSD=NA)
+  mutate(attrBurden = PAF * burden / 100) %>%
+  select(source, noiseMetric, shortName, outcome, PAF, attrBurden) %>%
+  mutate(dw = NA , HAorHSD = NA) 
 
-streetNoiseBurden2<-er_list %>%
+#Problem: die Autokorrelation ist nicht eingetragen#
+
+
+#-------------------------------------------------------#
+streetNoiseBurden2 <- er_list %>%
   filter(burdenCalculation == "total") %>%
-  aggregate(affected_i ~ source + noiseMetric + shortName + outcome,
-            data = .,
-            sum) %>%
+  group_by(source, noiseMetric, shortName,outcome) %>%
+  summarise(HAorHSD = sum(affected_i))%>%
   left_join(., dw_list, by = "outcome") %>%
-  mutate(HAorHSD = affected_i, .keep = "unused") %>%
-  mutate(attrBurden = HAorHSD * dw,PAF=NA)
+  mutate(attrBurden = HAorHSD * dw, PAF = NA)
 names(streetNoiseBurden1)
 names(streetNoiseBurden2)
-streetNoiseBurden <- rbind(streetNoiseBurden1,streetNoiseBurden2)
-pl3<-ggplot(streetNoiseBurden,aes(x=shortName,xlab=NULL,y=100000*attrBurden/popHessen,fill=shortName))+
-  theme(axis.text.x = element_blank())+
-  geom_col()+
-  facet_grid(cols = vars(source))+geom_text(label=streetNoiseBurden$shortName,size=3.5,angle=55)+
+streetNoiseBurden <- rbind(streetNoiseBurden1, streetNoiseBurden2)
+pl3 <-
+  ggplot(
+    streetNoiseBurden,
+    aes(
+      x = shortName,
+      xlab = NULL,
+      y = 100000 * attrBurden / popHessen,
+      fill = shortName
+    )
+  ) +
+  theme(axis.text.x = element_blank()) +
+  geom_col() +geom_errorbar(aes(ymin=errors_min(100000 * attrBurden / popHessen),ymax=errors_max(100000 * attrBurden / popHessen)))+
+  facet_grid(cols = vars(source)) + geom_text(label = streetNoiseBurden$shortName,
+                                              size = 3.5,
+                                              angle = 55) +
   xlab(NULL) + ylab("attributable Burden /100000")
 pl3
-ggsave("graphs/StreetNoiseBurden.pdf")
+#ggsave("graphs/StreetNoiseBurden.pdf")
